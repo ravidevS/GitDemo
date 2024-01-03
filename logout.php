169def2fa3a839829d611d7552eae82dbc3c9dbc
Ravidev S <ravidev.it@gmail.com>
2024-01-04 02:50:32 +0530

    Revert "Devlop local"

--- logout.php
+++ logout.php
@@ -3,10 +3,7 @@ session_start();
 session_unset();
 session_destroy();
 header('location:index.php');
-
-//dsjflkajflfassss
-//dsjflkajflfassss
-//dsjflkajflfassss
-//dsjflkajflfassss
+//dsfsadfsa
+//sdfsadfsdf
 
 ?>
